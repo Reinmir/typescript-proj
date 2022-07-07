c93d82bad5748cb6f9156c23b0195579113e698a
Reinmir <studenko.andrey@gmail.com>
2022-07-07 15:22:02 +0300

    444

--- src/constants/routeNames.ts
+++ src/constants/routeNames.ts
@@ -1,16 +1,16 @@
-export const peoplePage = '/people-page'
+export const peoplePage = "/people-page";
 
 export enum PageRoutes {
-  PeoplePage = '/people-page',
-  Login = '/login',
-  Register = '/register',
-  ToDos = '/todos',
-  ToDo = '/todos/:todoId',
-  Person = 'people-page/person',
-  ShoppingCart = '/shopping-cart',
-  PersonalCabinet = '/personal-cabinet',
-  Home = '/'
+  PeoplePage = "/people-page",
+  Login = "/login",
+  Register = "/register",
+  ToDos = "/todos",
+  ToDo = "/todos/:todoId",
+  Person = "people-page/person",
+  ShoppingCart = "/shopping-cart",
+  PersonalCabinet = "/personal-cabinet",
+  Home = "/",
 }
-export enum KeyLocalStorage{
-    User = 'user'
+export enum KeyLocalStorage {
+  User = "user",
 }
